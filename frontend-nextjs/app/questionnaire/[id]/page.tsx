984a4221da5626e43cbf4836f83dc4b58fe0f8cf
Lizéa <lizeadelay@gmail.com>
2025-11-27 21:05:39 +0000

    feat: display one questionnaire
    - show questionnaire inforamtions
    - show one question with its choices
    - button to return to the questionnaire list

---
+++ frontend-nextjs/app/questionnaire/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { fetchQuestionnaireById } from '@/app/services/api/questionnaires'
+import { Questionnaire } from '../../types/questionnaireType'
+import QuestionItem from '../../components/questionItem'
+
+export default async function QuestionnairePage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  const questionnaire: Questionnaire = await fetchQuestionnaireById(id)
+  const questions = questionnaire.questions
+
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-8 flex flex-col justify-center">
+      <h1 className="text-xl font-semibold text-white-900 mb-2">
+        {questionnaire.title}
+      </h1>
+      <p className="text-white-700 mb-4">{questionnaire.description}</p>
+      <QuestionItem urlQuestion={questions[0]} />
+      <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md m-4">
+        Retour à la liste de Questionnaires
+      </button>
+    </div>
+  )
+}
