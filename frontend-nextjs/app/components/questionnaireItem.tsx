e65552d16210506b832218db0e4382f46c391486
Lizéa <lizeadelay@gmail.com>
2025-11-27 21:00:36 +0000

    feat: create questionnaireItem

---
+++ frontend-nextjs/app/components/questionnaireItem.tsx
@@ -0,0 +1,23 @@
+import { Questionnaire } from '@/app/types/questionnaireType'
+export default async function QuestionnaireItem({
+  questionnaire,
+}: {
+  questionnaire: Questionnaire
+}) {
+  return (
+    <div>
+      <li
+        key={questionnaire.id}
+        className="bg-white shadow-md rounded-lg p-6 hover:shadow-xl"
+      >
+        <h2 className="text-xl font-semibold text-gray-900 mb-2">
+          {questionnaire.title}
+        </h2>
+        <p className="text-gray-700">{questionnaire.description}</p>
+        <p className="text-sm text-gray-500 mt-2">
+          Créé le: {new Date(questionnaire.creationDate).toLocaleDateString()}
+        </p>
+      </li>
+    </div>
+  )
+}
