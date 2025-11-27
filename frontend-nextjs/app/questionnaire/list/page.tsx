173b307456669888ed14634134ddd647dc3c9956
Lizéa <lizeadelay@gmail.com>
2025-11-27 21:01:49 +0000

    feat: use questionnaireItem in questionnaireListPage

--- frontend-nextjs/app/questionnaire/list/page.tsx
+++ frontend-nextjs/app/questionnaire/list/page.tsx
@@ -1,18 +1,23 @@
 import fetchAllQuestionnaires from '../../services/api/questionnaires';
 import { Questionnaire } from '../../types/questionnaireType';
+import QuestionnaireItem from '../../components/questionnaireItem';
 
-export default async function Page() {
-    const $data = await fetchAllQuestionnaires();
-    const $questionnaires: Questionnaire[] = $data.member
+export default async function QuestionnaireListPage() {
+    const data = await fetchAllQuestionnaires();
+    const questionnaires: Questionnaire[] = data.member
 
   return (
-    <div>
-      <h1>Questionnaire List Page</h1>
-      <ul>
-        { $questionnaires.map((questionnaire) => (
-            <li key={questionnaire.id}>
-                <h2>{questionnaire.title}</h2>
-            </li>))
+    <div className="max-w-4xl mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-center mb-8 text-white-800" >
+        Liste de Questionnaires
+      </h1>
+      <ul className="space-y-6">
+        { questionnaires.map((questionnaire) => (
+            <QuestionnaireItem 
+              key={questionnaire.id}
+              questionnaire={questionnaire}
+            />
+          ))
         }
       </ul>
     </div>
