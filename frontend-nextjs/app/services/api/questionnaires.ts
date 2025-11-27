e2f748d473224d88b0464d0d8409aebe12992f5a
Lizéa <lizeadelay@gmail.com>
2025-11-27 20:59:38 +0000

    feat: add fetchs to get one entity
    - fetch questionnaire with id
    - fetch question with url
    - fetch choice with url

--- frontend-nextjs/app/services/api/questionnaires.ts
+++ frontend-nextjs/app/services/api/questionnaires.ts
@@ -1,7 +1,25 @@
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = 'http://localhost:8000'
 
 export default async function fetchAllQuestionnaires() {
-    return fetch(`${BASE_URL}/questionnaires`).then((response) => {
-        return response.json();
-    });
+  return fetch(`${BASE_URL}/api/questionnaires`).then((response) => {
+    return response.json()
+  })
+}
+
+export async function fetchQuestionnaireById(id: string) {
+  return fetch(`${BASE_URL}/api/questionnaires/${id}`).then((response) => {
+    return response.json()
+  })
+}
+
+export async function fetchQuestionByUrl(urlQuestion: string) {
+  return fetch(`${BASE_URL}${urlQuestion}`).then((response) => {
+    return response.json()
+  })
+}
+
+export async function fetchChoiceByUrl(urlChoice: string) {
+  return fetch(`${BASE_URL}${urlChoice}`).then((response) => {
+    return response.json()
+  })
 }
