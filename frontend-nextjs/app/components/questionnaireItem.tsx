17e0a915b5fb0cfe4268c8231d401bf7f7bfff73
Lizéa <lizeadelay@gmail.com>
2025-11-27 23:35:44 +0000

    feat: add link to questionnaire/[id]

--- frontend-nextjs/app/components/questionnaireItem.tsx
+++ frontend-nextjs/app/components/questionnaireItem.tsx
@@ -10,13 +10,15 @@ export default async function QuestionnaireItem({
         key={questionnaire.id}
         className="bg-white shadow-md rounded-lg p-6 hover:shadow-xl"
       >
-        <h2 className="text-xl font-semibold text-gray-900 mb-2">
-          {questionnaire.title}
-        </h2>
-        <p className="text-gray-700">{questionnaire.description}</p>
-        <p className="text-sm text-gray-500 mt-2">
-          Créé le: {new Date(questionnaire.creationDate).toLocaleDateString()}
-        </p>
+      <a href={`/questionnaire/${questionnaire.id}`}>
+          <h2 className="text-xl font-semibold text-gray-900 mb-2">
+            {questionnaire.title}
+          </h2>
+          <p className="text-gray-700">{questionnaire.description}</p>
+          <p className="text-sm text-gray-500 mt-2">
+            Créé le: {new Date(questionnaire.creationDate).toLocaleDateString()}
+          </p>
+        </a>
       </li>
     </div>
   )
