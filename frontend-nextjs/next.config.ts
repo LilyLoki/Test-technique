e054c123f067e36526a671a5e16fae950a6d32a6
Lizéa <lizeadelay@gmail.com>
2025-11-28 07:37:07 +0000

    feat: put env variable in next.config

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL:
+      process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000',
+    NEXT_SERVER_BASE_URL:
+      process.env.NEXT_SERVER_BASE_URL || 'http://localhost:8000',
+  },
 }
 
 export default nextConfig
