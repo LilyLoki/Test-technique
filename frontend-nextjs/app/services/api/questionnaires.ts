957530f5bd005a04bfb7f0237523141e97b01cae
Lizéa <lizeadelay@gmail.com>
2025-11-28 07:37:07 +0000

    feat: put env variable in next.config

--- frontend-nextjs/app/services/api/questionnaires.ts
+++ frontend-nextjs/app/services/api/questionnaires.ts
@@ -1,4 +1,7 @@
-export const BASE_URL = 'http://localhost:8000'
+export const BASE_URL =
+  typeof window === 'undefined'
+    ? process.env.NEXT_SERVER_BASE_URL || 'http://backend:8000' // server-side (SSR)
+    : process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000' // client-side
 
 export default async function fetchAllQuestionnaires() {
   return fetch(`${BASE_URL}/api/questionnaires`).then((response) => {
