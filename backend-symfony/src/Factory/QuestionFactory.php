f94c879565b51c8e0827bdd83d3d0efd742e3b24
Lizéa <lizeadelay@gmail.com>
2025-11-28 08:57:18 +0000

    feat: add working url for mediaUrl in factory

--- backend-symfony/src/Factory/QuestionFactory.php
+++ backend-symfony/src/Factory/QuestionFactory.php
@@ -33,12 +33,26 @@ final class QuestionFactory extends PersistentProxyObjectFactory
     #[\Override]
     protected function defaults(): array|callable
     {
-        $mediaTypeList = ['image', 'video', 'audio', 'text'];
+        $mediaTypeList = ['image', 'video', 'text'];
         $mediaType = self::faker()->randomElement($mediaTypeList);
+
+        if ('video' === $mediaType) {
+            $videoUrls = [
+                'https://www.youtube.com/watch?v=JvSVXJ-3HaY',
+                'https://www.youtube.com/watch?v=3vLbEUXN7CY',
+                'https://www.youtube.com/watch?v=mYJ_7WCrLNI',
+            ];
+            $mediaUrl = self::faker()->randomElement($videoUrls);
+        }
+        if ('image' === $mediaType) {
+            $imageUrls = [
+                'https://picsum.photos/500',
+                'https://picsum.photos/800/600',
+            ];
+            $mediaUrl = self::faker()->randomElement($imageUrls);
+        }
         if ('text' === $mediaType) {
             $mediaUrl = null;
-        } else {
-            $mediaUrl = self::faker()->url();
         }
 
         return [
