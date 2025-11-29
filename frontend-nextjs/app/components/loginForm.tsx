e6afdb3c5409ff6d0dd694de802eb733a5fe3624
Lizéa <lizeadelay@gmail.com>
2025-11-29 04:24:08 +0000

    feat: add sumbit to LoginForm
    - connect the user

--- frontend-nextjs/app/components/loginForm.tsx
+++ frontend-nextjs/app/components/loginForm.tsx
@@ -1,9 +1,36 @@
 'use client'
 
+import { useState } from 'react'
+import { login } from '@/app/services/api/users'
+import { useRouter } from 'next/navigation'
+import { useAuth } from '../contexts/AuthContext'
+
 export default function LoginForm({}) {
-  
+  const { login } = useAuth()
+  const [error, setError] = useState<string | null>(null)
+  const router = useRouter()
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setError(null)
+
+    const formData = new FormData(event.currentTarget)
+    const username = formData.get('username')
+    const password = formData.get('password')
+
+    try {
+      await login(username, password)
+      router.push('/questionnaire/list')
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message)
+      }
+    }
+  }
+
   return (
     <form
+      onSubmit={handleSubmit}
       className="max-w-md mx-auto bg-white p-6 rounded shadow"
     >
       <h2 className="text-2xl text-black mb-4 font-bold text-center">
@@ -21,6 +48,7 @@ export default function LoginForm({}) {
         placeholder="Password"
         className="w-full text-black mb-4 p-2 border border-gray-300 rounded"
       />
+      <p className="text-red-500 mb-4 text-center">{error}</p>
       <button
         type="submit"
         className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
